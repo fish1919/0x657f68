69f936c615489d777485316b4df10c586393204d
evshiron <evshiron@gmail.com>
2016-05-06 21:39:56 +0800

    feat(BrowseDetails): add pagination

--- components/browse/details.js
+++ components/browse/details.js
@@ -2,6 +2,23 @@
 const Vue = require('vue');
 
 const template = `<div class="row">
+
+<nav class="col-md-offset-2 col-md-10">
+
+    <ul class="pagination">
+
+        <li v-bind:class="{ disabled: detailResult.PageIdx == 0 }"><a v-on:click="BrowsePrevious()">&larr;</a></li>
+
+        <li v-for="pageIdx of pageIndices" v-bind:class="{ active: pageIdx == detailResult.PageIdx }">
+            <a v-on:click="BrowseJump(pageIdx)">{{ pageIdx }}</a>
+        </li>
+
+        <li v-bind:class="{ disabled: detailResult.PageIdx == detailResult.PageCount - 1 }"><a v-on:click="BrowseNext()">&rarr;</a></li>
+
+    </ul>
+
+</nav>
+
 <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2" v-for="image of detailResult.Images">
 
     <a class="thumbnail" style="text-align: center;" v-link="{ path: '/browse/resources/' + encodeURIComponent(detailResult.Url) + '/' + detailResult.PageIdx + '/' + $index }" v-bind:style="{ height: imageHeight + 'px' }">
@@ -11,14 +28,38 @@ const template = `<div class="row">
     </a>
 
 </div>
+
 </div>`;
 
 module.exports = Vue.extend({
     template,
     data: () => ({}),
     computed: {
+        pageIndices: function() {
+            return this.detailResult.PageIdx || this.detailResult.PageIdx == 0 ? [0, 1, 2, 3, 4, 5, 6, 7, 8].map((val) => this.detailResult.PageIdx + val - 4).filter((val) => val >= 0 && val < this.detailResult.PageCount) : [];
+        },
         imageHeight: function() {
             return Math.max.apply(null, this.detailResult.Images.map((image) => image.Height)) + 8;
+        }
+    },
+    methods: {
+        BrowsePrevious: function() {
+            this.Detail(
+                this.detailResult.Url,
+                this.detailResult.PageIdx > 0 ? this.detailResult.PageIdx - 1 : 0
+            );
+        },
+        BrowseJump: function(pageIdx) {
+            this.Detail(
+                this.detailResult.Url,
+                pageIdx
+            );
+        },
+        BrowseNext: function() {
+            this.Detail(
+                this.detailResult.Url,
+                this.detailResult.PageIdx < this.detailResult.PageCount - 1 ? this.detailResult.PageIdx + 1 : this.detailResult.PageIdx
+            );
         }
     },
     route: {
